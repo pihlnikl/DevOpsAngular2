e466828cae7f6ae22a118fb0fd5003f3a1f38433
pihlnikl <niklas_pihl@hotmail.com>
2019-09-24 13:48:11 +0300

    first commit

---
+++ src/app/components/quote-text/quote-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-text',
+  templateUrl: './quote-text.component.html',
+  styleUrls: ['./quote-text.component.scss']
+})
+export class QuoteTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
